a6bbf059ef05970d73cb5e47497c49a3f86cf8b1
Kanaanwong <kanaanwong@gmail.com>
2021-08-31 14:29:36 +0800

    revert change something

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KanaanDoc'
+copyright = '2021, Kanaanwong'
+author = 'Kanaanwong'
 
 release = '0.1'
 version = '0.1.0'
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -13,6 +13,7 @@ how to :ref:`installation` the project.
 
    This project is under active development.
    Lumache has its documentation hosted on Read the Docs.
+
 Contents
 --------
 
